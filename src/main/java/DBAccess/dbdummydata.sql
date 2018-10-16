25d878104becdb8735eedddfff5b4c2e3f60a74e
Mark <mark.d.denner@gmail.com>
2018-10-16 02:17:56 +0200

    updating project

---
+++ src/main/java/DBAccess/dbdummydata.sql
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Mark
+ * Created: 16-Oct-2018
+ */
+
+
+
+Insert into Users(`email`, `password`, `role`) Values('cus@somewhere.com','1234','customer');
+Insert into Users(`email`, `password`, `role`) Values('Emp@company.com','1234','employee');
+
+
+
